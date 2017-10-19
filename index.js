eeda4215a132f01a382918f8240e6d1e8f004f85
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 17:44:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear() - 1
+  }
+}
 
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let horizonallyTravelled = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+		let verticallyTravelled = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+		return horizonallyTravelled + verticallyTravelled;
+  }
+
+  estimatedTime(peakTime) {
+    if (peakTime) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
